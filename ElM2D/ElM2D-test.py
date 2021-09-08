f90a99a5717097205726b62443e37db27cd0e4dd
sgbaird <sterling.baird@icloud.com>
2021-09-07 23:14:42 -0600

    Update ElM2D-test.py

--- ElM2D/ElM2D-test.py
+++ ElM2D/ElM2D-test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 12 16:14:14 2021
+Created on Thu Aug 12 16:14:14 2021.
 
 @author: sterg
 """
@@ -10,10 +10,20 @@ import pandas as pd
 from ElM2D import ElM2D
 
 def main():
+    """
+    Run ElM2D fitting which is compatible with Spyder for debugging purposes.
+
+    Returns
+    -------
+    None.
+
+    """
     mapper = ElM2D()
-    datapath = join("train-debug.csv")
+    #datapath = join("..","..","discovery","ael_bulk_modulus_voigt","train.csv")
+    datapath = "train-debug.csv"
     df = pd.read_csv(datapath)
     mapper.fit(df["formula"])
+    mapper.dm
     
 if __name__ == '__main__':
     freeze_support()
