f5737e73f31c09e0e1386028c51dc3dfc3acb31e
sgbaird <sterling.baird@icloud.com>
2021-10-15 03:37:30 -0600

    Update mat_discover_.py

--- mat_discover/mat_discover_.py
+++ mat_discover/mat_discover_.py
@@ -17,7 +17,7 @@ Created on Mon Sep  6 23:15:27 2021.
 import os
 from warnings import warn
 from operator import attrgetter
-from ElM2D.utils.Timer import Timer, NoTimer
+from ..ElM2D.utils.Timer import Timer, NoTimer
 
 import matplotlib.pyplot as plt
 
@@ -36,7 +36,7 @@ from sklearn.metrics import mean_squared_error
 import umap
 import hdbscan
 
-from ElM2D.ElM2D_ import ElM2D
+from ..ElM2D.ElM2D_ import ElM2D
 
 from mat_discover.utils.nearest_neigh import nearest_neigh_props
 from mat_discover.utils.pareto import pareto_plot, get_pareto_ind
