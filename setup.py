fab78a22de8421dd0c7aa918024c656efde02820
Sterling Baird <45469701+sgbaird@users.noreply.github.com>
2021-10-22 03:07:58 -0600

    create dummy file setup.py for used by counter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+raise Exception(
+    """
+    This file exists to set up the Github "Used By" counter properly and is not meant to be run.
+    """
+)
+
+setup(name="mat_discover")
