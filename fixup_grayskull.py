58212721a8179b82e41bbc818c53bb2da96e1a94
Sterling Baird <45469701+sgbaird@users.noreply.github.com>
2021-10-29 11:39:56 -0600

    add pytorch and cudatoolkit deps

--- fixup_grayskull.py
+++ fixup_grayskull.py
@@ -6,4 +6,6 @@ my_recipe["requirements"]["host"].append("flit")
 del my_recipe["requirements"]["build"]
 my_recipe["requirements"]["run"].remove("kaleido")
 my_recipe["requirements"]["run"].append("python-kaleido")
+my_recipe["requirements"]["run"].append("pytorch==1.10.*")
+my_recipe["requirements"]["run"].append("cudatoolkit==11.3.*")
 my_recipe.save()
