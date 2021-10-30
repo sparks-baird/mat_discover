5b6496e0cb4cde2d64b8903327279216cd342df7
sgbaird <sterling.baird@icloud.com>
2021-10-30 14:22:55 -0600

    Update fixup_grayskull.py

--- fixup_grayskull.py
+++ fixup_grayskull.py
@@ -1,11 +1,15 @@
 """Touch up the conda recipe from grayskull using conda-souschef."""
 from souschef.recipe import Recipe
+from os.path import join
 
-my_recipe = Recipe(load_file="meta.yaml")
+fpath = join("mat_discover", "meta.yaml")
+fpath2 = join("scratch", "meta.yaml")
+my_recipe = Recipe(load_file=fpath)
 my_recipe["requirements"]["host"].append("flit")
 del my_recipe["requirements"]["build"]
 my_recipe["requirements"]["run"].remove("kaleido")
 my_recipe["requirements"]["run"].append("python-kaleido")
 my_recipe["requirements"]["run"].append("pytorch >=1.9.0")
 my_recipe["requirements"]["run"].append("cudatoolkit ==11.3.1")
-my_recipe.save()
+my_recipe.save(fpath)
+my_recipe.save(fpath2)
