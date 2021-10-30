12aa6a15bb3195b1ef75a237bffa11367bcc07de
sgbaird <sterling.baird@icloud.com>
2021-10-30 16:23:39 -0600

    cudatoolkit <11.4

---
+++ run_grayskull.py
@@ -0,0 +1,20 @@
+"""Touch up the conda recipe from grayskull using conda-souschef."""
+from souschef.recipe import Recipe
+import os
+from os.path import join
+import mat_discover
+
+os.system("grayskull pypi mat_discover=={}".format(mat_discover.__version__))
+
+fpath = join("mat_discover", "meta.yaml")
+fpath2 = join("scratch", "meta.yaml")
+my_recipe = Recipe(load_file=fpath)
+my_recipe["requirements"]["host"].append("flit")
+my_recipe["build"]["noarch"] = "python"
+del my_recipe["requirements"]["build"]
+my_recipe["requirements"]["run"].remove("kaleido")
+my_recipe["requirements"]["run"].append("python-kaleido")
+my_recipe["requirements"]["run"].append("pytorch >=1.9.0")
+my_recipe["requirements"]["run"].append("cudatoolkit <11.4")
+my_recipe.save(fpath)
+my_recipe.save(fpath2)
