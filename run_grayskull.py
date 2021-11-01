edbce3e5892ec2727c24e60411d5afafdf453ac7
sgbaird <sterling.baird@icloud.com>
2021-11-01 08:56:58 -0600

    Update run_grayskull.py

--- run_grayskull.py
+++ run_grayskull.py
@@ -10,15 +10,14 @@ os.system("grayskull pypi {0}=={1}".format(name, version))
 
 Path("scratch").mkdir(exist_ok=True)
 
-fpath = join("mat_discover", "meta.yaml")
+fpath = join(name, "meta.yaml")
 fpath2 = join("scratch", "meta.yaml")
 my_recipe = Recipe(load_file=fpath)
 my_recipe["requirements"]["host"].append("flit")
 my_recipe["build"].add_section("noarch")
 my_recipe["build"]["noarch"] = "python"
 del my_recipe["requirements"]["build"]
-my_recipe["requirements"]["run"].remove("kaleido")
-my_recipe["requirements"]["run"].append("python-kaleido")
+my_recipe["requirements"]["run"].replace("kaleido", "python-kaleido")
 my_recipe["requirements"]["run"].append("pytorch >=1.9.0")
 my_recipe["requirements"]["run"].append("cudatoolkit <11.4")
 my_recipe.save(fpath)
