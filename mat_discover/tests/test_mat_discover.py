5d39f99568f387104cb35e79cafd469ee3a6297c
sgbaird <sterling.baird@icloud.com>
2021-12-11 02:10:38 -0700

    suggest_next_experiment()

--- mat_discover/tests/test_mat_discover.py
+++ mat_discover/tests/test_mat_discover.py
@@ -31,5 +31,6 @@ def test_mat_discover():
     disc.group_cross_val(cat_df, umap_random_state=42)
     print("scaled test error = ", disc.scaled_error)
     disc.plot()
+    next_experiment = disc.suggest_next_experiment()
     # disc.save() #doesn't work with pytest for some reason (pickle: object not the same)
     # disc.load()
