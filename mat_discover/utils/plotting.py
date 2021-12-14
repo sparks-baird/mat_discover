bd1fc73e5085cc1a895119ba1853799638d13a59
sgbaird <sterling.baird@icloud.com>
2021-12-14 02:18:25 -0700

    create matplotlibify as stand-alone function

--- mat_discover/utils/plotting.py
+++ mat_discover/utils/plotting.py
@@ -245,3 +245,56 @@ def group_cv_parity(ytrue, ypred, labels, figure_dir="figures"):
     plt.savefig(join(figure_dir, "group-cv-parity"))
     plt.show()
     return fig
+
+
+def matplotlibify(fig, size=24):
+    # make it look more like matplotlib
+    # modified from: https://medium.com/swlh/formatting-a-plotly-figure-with-matplotlib-style-fa56ddd97539)
+    font_dict = dict(family="Arial", size=size, color="black")
+
+    # app = QApplication(sys.argv)
+    # screen = app.screens()[0]
+    # dpi = screen.physicalDotsPerInch()
+    # app.quit()
+    dpi = 142
+
+    width_inches = 3.5
+    height_inches = width_inches
+
+    fig.update_layout(
+        font=font_dict,
+        plot_bgcolor="white",
+        width=width_inches * dpi,
+        height=height_inches * dpi,
+        margin=dict(r=40, t=20, b=10),
+    )
+
+    fig.update_yaxes(
+        showline=True,  # add line at x=0
+        linecolor="black",  # line color
+        linewidth=2.4,  # line size
+        ticks="inside",  # ticks outside axis
+        tickfont=font_dict,  # tick label font
+        mirror="allticks",  # add ticks to top/right axes
+        tickwidth=2.4,  # tick width
+        tickcolor="black",  # tick color
+    )
+
+    fig.update_xaxes(
+        showline=True,
+        showticklabels=True,
+        linecolor="black",
+        linewidth=2.4,
+        ticks="inside",
+        tickfont=font_dict,
+        mirror="allticks",
+        tickwidth=2.4,
+        tickcolor="black",
+    )
+    fig.update(layout_coloraxis_showscale=False)
+
+    width_default_px = fig.layout.width
+    targ_dpi = 300
+    scale = width_inches / (width_default_px / dpi) * (targ_dpi / dpi)
+
+    return fig, scale
