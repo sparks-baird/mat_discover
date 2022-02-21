ce2ae8d5e37b2753db31420a1979310e5c3ea11a
Sterling Baird <45469701+sgbaird@users.noreply.github.com>
2022-02-21 14:47:29 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,12 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []  # throws a WARNING, removed "_static"
+html_static_path = ['_static']
+html_logo = "discover-logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 html_extra_path = ["googlea441f484329a8f75.html"]
 
