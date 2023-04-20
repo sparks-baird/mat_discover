97c6ea50234f502778508633c4a10f5e47c09726
sgbaird <sterling.baird@utah.edu>
2023-04-19 22:18:17 -0600

    issue with matminer dep
    ```
    mat\_discover.adaptive\_design\_ module
    ------------------------------------
    WARNING: autodoc: failed to import module 'adaptive_design' from module 'mat_discover'; the following exception was raised:
    Traceback (most recent call last):
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/envs/latest/lib/python3.9/site-packages/sphinx/ext/autodoc/importer.py", line 58, in import_module
        return importlib.import_module(modname)
      File "/home/docs/.asdf/installs/python/3.9.15/lib/python3.9/importlib/__init__.py", line 127, in import_module
        return _bootstrap._gcd_import(name[level:], package, level)
      File "<frozen importlib._bootstrap>", line 1030, in _gcd_import
      File "<frozen importlib._bootstrap>", line 1007, in _find_and_load
      File "<frozen importlib._bootstrap>", line 986, in _find_and_load_unlocked
      File "<frozen importlib._bootstrap>", line 680, in _load_unlocked
      File "<frozen importlib._bootstrap_external>", line 850, in exec_module
      File "<frozen importlib._bootstrap>", line 228, in _call_with_frames_removed
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/checkouts/latest/mat_discover/adaptive_design.py", line 6, in <module>
        from mat_discover.mat_discover_ import Discover, my_mvn
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/checkouts/latest/mat_discover/mat_discover_.py", line 18, in <module>
        from matbench_genmetrics.utils.featurize import cdvae_cov_struct_fingerprints
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/envs/latest/lib/python3.9/site-packages/matbench_genmetrics/utils/featurize.py", line 13, in <module>
        from matbench_genmetrics.utils.match import get_tqdm
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/envs/latest/lib/python3.9/site-packages/matbench_genmetrics/utils/match.py", line 73, in <module>
        CrystalNNFP = CrystalNNFingerprint.from_preset("ops")
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/envs/latest/lib/python3.9/site-packages/matminer/featurizers/site/fingerprint.py", line 397, in from_preset
        return CrystalNNFingerprint(op_types, chem_info=None, **kwargs)
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/envs/latest/lib/python3.9/site-packages/matminer/featurizers/site/fingerprint.py", line 422, in __init__
        cn_motif_op_params = load_cn_motif_op_params()
      File "/home/docs/checkouts/readthedocs.org/user_builds/mat-discover/envs/latest/lib/python3.9/site-packages/matminer/featurizers/site/fingerprint.py", line 874, in load_cn_motif_op_params
        with open(
    FileNotFoundError: [Errno 2] No such file or directory: '/dev/cn_opt_params.yaml'
    ```

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,14 @@ html_theme_options = {
 
 html_extra_path = ["googlea441f484329a8f75.html"]
 
-autodoc_mock_imports = ["numba", "torch", "pymatgen", "hdbscan"]
+autodoc_mock_imports = [
+    "numba",
+    "torch",
+    "pymatgen",
+    "hdbscan",
+    "matbench_genmetrics",
+    "matminer",
+]
 
 # https://github.com/sphinx-doc/sphinx/issues/7000#issuecomment-677916705
 source_suffix = [".rst", ".md"]
